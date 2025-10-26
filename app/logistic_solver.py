4a041538319b024cb1a3d5900cab04856d78224c
hmcguire0583 <harryjunior2026@gmail.com>
2025-10-26 11:40:46 -0400

    changes Co-authored-by: Joao Z <GhostCinna@users.noreply.github.com>

--- app/logistic_solver.py
+++ app/logistic_solver.py
@@ -7,7 +7,7 @@ def build_graph(locations, roads):
     return graph
 
 def dijkstra(graph, start):
-    queue = [(0, start, [])]
+    queue = [(0, start, [start])]
     visited = set()
     min_dists = {start: 0}
     paths = {start: [start]}
@@ -28,43 +28,54 @@ def compute_routes(data):
     locations = data["locations"]
     roads = data["roads"]
     graph = build_graph(locations, roads)
-    depot_id = 0  # Depot is always id=0 in your file
-    trucks = ["Truck 1", "Truck 2", "Truck 3"]
+    depot_id = 0
+    trucks = [f"Truck {i+1}" for i in range(data["meta"]["trucks"])]
     truck_capacity = data["meta"]["truck_capacity"]
 
     supply_locs = [loc for loc in locations if loc["demand"] > 0]
-    supply_queue = []
-    for loc in supply_locs:
-        supply_queue += [loc["id"]] * loc["demand"]
-
-    result = []
+    demand_map = {loc["id"]: loc["demand"] for loc in supply_locs}
     supplies_delivered = {loc["name"]: 0 for loc in supply_locs}
     truck_usage = {t: 0 for t in trucks}
     truck_routes = {t: [] for t in trucks}
     truck_idx = 0
 
-    while supply_queue:
+    while any(demand_map.values()):
         truck = trucks[truck_idx % len(trucks)]
         truck_load = 0
         curr = depot_id
         route = [depot_id]
         total_time = 0
-        while supply_queue and truck_load < truck_capacity:
-            # Find closest supply location still needing goods
+        visited_this_run = set()
+
+        while truck_load < truck_capacity and any(demand_map.values()):
             dists, paths = dijkstra(graph, curr)
-            candidates = [(dists[l], l) for l in set(supply_queue) if l in dists]
+            candidates = [
+                (dists[l], l) for l in demand_map
+                if demand_map[l] > 0 and l in dists and l not in visited_this_run
+                and demand_map[l] <= (truck_capacity - truck_load)
+            ]
             if not candidates:
                 break
             dist, chosen = min(candidates)
-            truck_load += 1
+            deliverable = demand_map[chosen]
+            truck_load += deliverable
+            demand_map[chosen] = 0
             loc_name = next(loc["name"] for loc in supply_locs if loc["id"] == chosen)
-            supplies_delivered[loc_name] += 1
+            supplies_delivered[loc_name] += deliverable
             route += paths[chosen][1:]  # Skip current node
             total_time += dist
             curr = chosen
-            supply_queue.remove(chosen)
+            visited_this_run.add(chosen)
+
+        # Return to depot
+        if curr != depot_id:
+            dists_back, paths_back = dijkstra(graph, curr)
+            route += paths_back[depot_id][1:]
+            total_time += dists_back[depot_id]
+
         truck_usage[truck] += 1
         truck_routes[truck].append((route, total_time))
+        truck_idx += 1
 
     # Output formatting
     route_text = []
